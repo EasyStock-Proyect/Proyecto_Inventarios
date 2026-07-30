11a782039d511cafb2198e8017d5cc0d80766454
Samuel Gaviria Morales <sagamo2504@hotmail.com>
2026-07-30 13:44:09 -0500

    feat(US-14): enhance category management with update and delete functionalities, including error handling and success messages

--- backend/src/services/category.service.js
+++ backend/src/services/category.service.js
@@ -65,7 +65,106 @@ async function createCategory(userId, data) {
 
 }
 
+async function updateCategory(userId, categoryId, data) {
+
+    const category = await prisma.category.findUnique({
+
+        where: {
+            id: categoryId,
+            userId
+        }
+
+    });
+
+    if (!category) {
+        throw new Error("Categoría no encontrada");
+    }
+
+    if (!data.name || !data.name.trim()) {
+        throw new Error("El nombre de la categoría es obligatorio");
+    }
+
+    const existingCategory = await prisma.category.findFirst({
+
+        where: {
+            userId,
+            name: data.name.trim(),
+
+            NOT: {
+                id: categoryId
+            }
+        }
+
+    });
+
+    if (existingCategory) {
+        throw new Error(
+            "Ya existe una categoría con ese nombre."
+        );
+    }
+
+    const updatedCategory = await prisma.category.update({
+
+        where: {
+            id: categoryId
+        },
+
+        data: {
+            name: data.name.trim()
+        }
+
+    });
+
+    return updatedCategory;
+
+}
+
+async function deleteCategory(userId, categoryId) {
+
+    const category = await prisma.category.findFirst({
+
+        where: {
+            id: categoryId,
+            userId
+        }
+
+    });
+
+    if (!category) {
+        throw new Error("Categoría no encontrada");
+    }
+
+    const product = await prisma.product.findFirst({
+
+        where: {
+            categoryId: categoryId,
+            userId
+        }
+
+    });
+
+    if (product) {
+        throw new Error(
+            "No se puede eliminar la categoría porque tiene productos asociados."
+        );
+    }
+
+
+    const deletedCategory = await prisma.category.delete({
+
+        where: {
+            id: categoryId
+        }
+
+    });
+
+    return deletedCategory;
+
+};
+
 module.exports = {
     getCategories,
-    createCategory
+    createCategory,
+    updateCategory,
+    deleteCategory
 };
