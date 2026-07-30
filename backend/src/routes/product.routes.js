218417ffe0fcadf1be7cf377c1d41afd8a10a185
Samuel Gaviria Morales <sagamo2504@hotmail.com>
2026-07-30 15:19:28 -0500

    feat(US-09): implement controller and routes for product CRUD operations with create, read, update, and delete functionalities

--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,13 +1,32 @@
 const express = require("express");
-const authMiddleware = require("../middlewares/auth.middleware");
-
 const router = express.Router();
 
-router.get("/", authMiddleware, (req, res) => {
-    res.json({
-        message: "Ruta protegida funcionando.",
-        user: req.user
-    });
-});
+const productController = require("../controllers/product.controller");
+const authMiddleware = require("../middlewares/auth.middleware");
+
+
+router.post(
+    "/", 
+    authMiddleware.verifyToken,
+    productController.createProduct
+);
+
+router.get(
+    "/",
+    authMiddleware.verifyToken,
+    productController.getAllProducts
+);
+
+router.put(
+    "/:id",
+    authMiddleware.verifyToken,
+    productController.updateProduct
+);
+
+router.delete(
+    "/:id",
+    authMiddleware.verifyToken,
+    productController.deleteProduct
+);
 
 module.exports = router;
