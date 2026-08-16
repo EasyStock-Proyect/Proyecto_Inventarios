9a38e6d374c052593cada4ecf1c2fc5c2561a231
Samuel Gaviria Morales <sagamo2504@hotmail.com>
2026-08-16 14:28:01 -0500

    Add comment to test GitHub Actions workflow

--- backend/index.js
+++ backend/index.js
@@ -7,3 +7,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en el puerto ${PORT}`);
 });
+
+// Esto es para testear el workflow de github actions
